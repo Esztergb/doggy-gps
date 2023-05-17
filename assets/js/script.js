ee01618e6e4f998bd4ff475508ab48228fd8fc30
Eszter Barbuscia <gubiscom@yahoo.com>
2023-05-16 19:06:00 -0700

    photo display update

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,16 +130,28 @@ var getPets = function () {
         <div class="row">
           <div class="col s6">
             <h5>${data.name} (${data.age})</h5>
-            <h6>${data.gender}</h6>
             <h6 class=text>${data.breeds.primary}</h6>
-            <p>${data.contact.address.address1} ${data.contact.address.city} ${data.contact.address.state} ${data.contact.address.postcode}</p>
+            <h6>${data.gender}</h6>
+            <p>${data.contact.address.address1} ${data.contact.address.city} ${
+        data.contact.address.state
+      } ${data.contact.address.postcode}</p>
             <ul class="list-group">
-            ${data.contact.phone ? `<li class=list-groiup-item>Phone: ${data.contact.phone}</li>` : ``}
-            ${data.contact.email ? `<li class=list-groiup-item>Email: ${data.contact.email}</li>` : ``}
+            ${
+              data.contact.phone
+                ? `<li class=list-groiup-item>Phone: ${data.contact.phone}</li>`
+                : ``
+            }
+            ${
+              data.contact.email
+                ? `<li class=list-groiup-item>Email: ${data.contact.email}</li>`
+                : ``
+            }
             <li class=list-groiup-item>Shelter ID: ${data.organization_id}</li>
           </div>
           <div class="col s6">
-          <img class="" src="${data.primary_photo_cropped.small}">
+          <img class="responsive-img circle" src="${
+            data.primary_photo_cropped.small
+          }">
           </div>
         </div>
       `;
